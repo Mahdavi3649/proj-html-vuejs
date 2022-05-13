96ef35e6be49fcb29c583ae49f7536a9096e6f79
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-13 11:23:38 +0200

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <header></header>
+    <main></main>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/style.scss";
+</style>
