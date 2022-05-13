95821ead9e1254b3e33917b7bc1d954377e3eaa3
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-13 12:41:56 +0200

    Add fontawesome and working header site

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-    <header></header>
+    <header>
+      <div
+        class="site-header d-flex justify-content-between align-items-center"
+      >
+        <div class="logo">
+          <img src="@/assets/img/avada-bakery-logo.png" alt="" />
+        </div>
+        <div class="menu-header">
+          <a v-for="item in menu" :key="item.id" :href="item.href">
+            {{ item.text }}
+          </a>
+          <font-awesome-icon
+            icon="fa-solid fa-cart-shopping"
+            style="color: #5b2ca2"
+          />
+        </div>
+      </div>
+    </header>
     <main></main>
     <footer></footer>
   </div>
@@ -10,6 +27,52 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      menu: [
+        {
+          id: 1,
+          href: "#",
+          text: "home",
+        },
+        {
+          id: 2,
+          href: "#",
+          text: "shop",
+        },
+        {
+          id: 3,
+          href: "#",
+          text: "about",
+        },
+        {
+          id: 4,
+          href: "#",
+          text: "gallery",
+        },
+        {
+          id: 5,
+          href: "#",
+          text: "location",
+        },
+        {
+          id: 6,
+          href: "#",
+          text: "journal",
+        },
+        {
+          id: 7,
+          href: "#",
+          text: "contact",
+        },
+        {
+          id: 8,
+          href: "#",
+          text: "my account",
+        },
+      ],
+    };
+  },
 };
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import specific icons */
+import { faCartShopping as fasCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* add icons to the library */
+library.add(fasCartShopping)
+
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+
+
+
 const bootstrap = require('bootstrap')
 Vue.use(bootstrap)
 
